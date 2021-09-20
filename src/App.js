f279bea2e7b308d66891ce108775d8add13ac114
dani-nawaz <daninawaz9@gmail.com>
2021-09-20 16:13:24 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useRef, useState } from 'react'
+import { ReactComponent as BoltIcon } from './icons/bolt.svg'
+import { ReactComponent as BellIcon } from './icons/bell.svg'
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg'
+import { ReactComponent as CaretIcon } from './icons/caret.svg'
+import { ReactComponent as CogIcon } from './icons/cog.svg'
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg'
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg'
+import { CSSTransition } from 'react-transition-group'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Navbar>
+      <NavItem icon={<BoltIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  )
 }
 
-export default App;
+export default App
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>{props.children}</ul>
+    </nav>
+  )
+}
+function NavItem(props) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <li className='nav-item'>
+      <a
+        href='#'
+        className='icon-button'
+        onClick={() => {
+          setOpen(!open)
+        }}
+      >
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  )
+}
+
+const DropdownMenu = () => {
+  const [activeMenu, setActiveMenu] = useState('main')
+  const [menuHeight, setMenuHeight] = useState(null)
+  const dropdownRef = useRef(null)
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
+  const calcHeight = (el) => {
+    const height = el.offsetHeight
+    setMenuHeight(height)
+  }
+
+  const DropdownItem = (props) => {
+    return (
+      <a
+        href='#'
+        className='menu-item'
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className='icon-button'>{props.leftIcon}</span>
+        {props.children}
+        {props.rightIcon && (
+          <span className='icon-right'>{props.rightIcon}</span>
+        )}
+        {/* <span className='icon-right'>{props.rightIcon}</span> */}
+      </a>
+    )
+  }
+
+  return (
+    <div className='dropdown' style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        unmountOnExit
+        timeout={500}
+        onEnter={calcHeight}
+        classNames='menu-primary'
+      >
+        <div className='menu'>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu='setting'
+          >
+            Setting
+          </DropdownItem>
+          <DropdownItem
+            leftIcon='🦧'
+            rightIcon={<ChevronIcon />}
+            goToMenu='animals'
+          >
+            Animals
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === 'setting'}
+        unmountOnExit
+        timeout={500}
+        onEnter={calcHeight}
+        classNames='menu-secondary'
+      >
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<ArrowIcon />}>
+            {' '}
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames='menu-secondary'
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<ArrowIcon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon='🦘'>Kangaroo</DropdownItem>
+          <DropdownItem leftIcon='🐸'>Frog</DropdownItem>
+          <DropdownItem leftIcon='🦋'>Horse?</DropdownItem>
+          <DropdownItem leftIcon='🦔'>Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  )
+}
